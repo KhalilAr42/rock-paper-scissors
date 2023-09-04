8644ebdbe4e22f19f51c4391c69f947d404da23a
Khalilar <khalilarchkak@gmail.com>
2023-09-04 10:11:37 +0100

    Add a function playRound : Takes player and computer selection and return the winner

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ function getComputerChoice() {
     // then return the random value
     return choices[random_index];
 }
+
+function playRound(playerSelection, computerSelection) {
+    // I need to compare the playerSelection and the computerSelection
+    // Based on that comparison I give a winner
+    // There are a lot of possible combinations
+    // I need to find a way to minimize that
+
+    //Define all possible cases where the player win
+    playerWinsWithRock = playerSelection == "rock" && computerSelection == "scissors";
+    playerWinsWithPaper = playerSelection == "paper" && computerSelection == "rock";
+    playerWinsWithScisors = playerSelection == "scissors" && computerSelection == "paper";
+
+    // If this boolean has any true then the player won, else the computer won!
+    playerWin = playerWinWithRock || playerWinWithPaper || playerWinWithScisors
+
+    // If the player and computer have the same selection then it's draw no need to verify anything
+    if(playerSelection === computerSelection) {
+        return "Draw"
+    } 
+
+    if(playerWin) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You Lost! ${computerSelection} beats ${playerSelection}`
+    }
+
+}
