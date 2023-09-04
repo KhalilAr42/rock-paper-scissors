1d9b360ed1bb9eb44e5200533262d09824644db0
Khalilar <khalilarchkak@gmail.com>
2023-09-04 11:14:26 +0100

    Add a function game : It will play 5 games and show us the result

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function playRound(playerSelection, computerSelection) {
     playerWinsWithScisors = playerSelection == "scissors" && computerSelection == "paper";
 
     // If this boolean has any true then the player won, else the computer won!
-    playerWin = playerWinWithRock || playerWinWithPaper || playerWinWithScisors
+    playerWin = playerWinsWithRock || playerWinsWithPaper || playerWinsWithScisors
 
     // If the player and computer have the same selection then it's draw no need to verify anything
     if(playerSelection === computerSelection) {
@@ -37,3 +37,38 @@ function playRound(playerSelection, computerSelection) {
     }
 
 }
+
+function game() {
+
+    // I need a variable to keep track of the player score and the computer score
+    // I need a variable to put in it the selected choice of the player
+    // I need to loop 5 times 
+    
+    let playerScore = 0;
+    let computerScore = 0;
+
+    
+    let result;
+    for (let index = 0; index < 5; index++) {
+        let playerChoice = prompt("Please choose a value between : rock,paper,scissors");
+        result = playRound(playerChoice,getComputerChoice());
+
+        console.log(result);
+        
+        if (result.split(" ")[1] === "Win!") {
+            playerScore++;
+        } else {
+            if (result != "Draw"){
+                computerScore++;
+            }
+        }
+        
+    }
+
+    console.log(`Final score :
+    player score : ${playerScore}
+    computer score : ${computerScore}`)
+
+}
+
+game()
